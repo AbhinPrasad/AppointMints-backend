ce01b0ca7c75eca4c0a30c01eb2f28dc0a0b0339
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-08-10 00:59:13 +0530

    app initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { appConfig, dbConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [dbConfig, appConfig] }),
+    TypeOrmModule.forRootAsync(dbConfig.asProvider()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/constants/index.ts
@@ -0,0 +1,1 @@
+export { default as constants } from './data.constants';
